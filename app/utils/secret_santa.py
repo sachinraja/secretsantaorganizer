616a8993f882bb57b391040ea303d31621584480
Sachin Raja <sachinraja2349@gmail.com>
2020-12-24 22:02:10 -0800

    Update matching system

--- app/utils/secret_santa.py
+++ app/utils/secret_santa.py
@@ -21,31 +21,40 @@ def match_people(start_people):
     random.shuffle(start_people)
     # sort by number of restrictions: max to min
     people = sorted(start_people, key=lambda person: len(person.restrictions), reverse=True)
-
+    potential_matches = people.copy()
     matches = {}
 
+    # iterate from most restrictions to least restrictions, 
+    # trying to match most restricted people first
     for person in people:
-
-        iteration_count = 0
-        # iterate from most restrictions to least restrictions, 
-        # trying to match most restricted people first
-        for potential_match in people:
-            # if potential_match is not also the person trying to be matched and 
-            # not already matched and 
+        for i, potential_match in enumerate(potential_matches):
+            # if potential_match is not also the person trying to be matched and
             # is not restricted and 
             # does not have the person trying to get matched
-            if (potential_match != person and 
-            potential_match not in matches.values() and 
-            potential_match.name not in person.restrictions and 
+            if (potential_match != person and
+            potential_match.name not in person.restrictions and
             matches.get(potential_match) != person):
                 matches[person] = potential_match
-                break
 
-            if iteration_count == len(people) - 1:
-                matches[person] = "Unmatched"
-                return None
+                potential_matches.pop(i)
+                break
             
-            iteration_count += 1
+            # person could not be matched
+            if i == len(potential_matches) - 1:
+                # attempt to replace from an earlier match
+                for i, (gifter, recipient) in enumerate(matches.items()):
+                    if (person != recipient and
+                    recipient.name not in person.restrictions and
+                    matches.get(recipient) != person and
+                    person != gifter and
+                    person not in gifter.restrictions):
+                        matches[gifter] = person
+                        matches[person] = recipient
+                        break
+                    
+                    # no replacements could be made
+                    if i == len(matches) - 1:
+                        return None
     
     return matches
 
@@ -105,11 +114,17 @@ def get_participants(form):
     """Get up to 100 participants from an input form."""
 
     participants = []
+    null_participants = 0
     for i in range(100):
         participant = form.getlist(f"participant{i}")
 
         if participant == []:
-            break
+            null_participants += 1
+
+            if null_participants >= 5:
+                break
+            
+            continue
 
         name, email, restrictions = participant
         
